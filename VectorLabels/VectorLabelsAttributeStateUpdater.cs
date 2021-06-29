daf35838af3312bf3983a7d3ef05b7451d3c533b
Antonio Rafael Antunes Miranda <36130345+Schwapo@users.noreply.github.com>
2021-06-29 17:07:12 +0200

    Update VectorLabelsAttributeStateUpdater.cs

--- VectorLabels/VectorLabelsAttributeStateUpdater.cs
+++ VectorLabels/VectorLabelsAttributeStateUpdater.cs
@@ -1,11 +1,40 @@
 using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector.Editor.ValueResolvers;
+using System.Collections.Generic;
 
 [assembly: RegisterStateUpdater(typeof(VectorLabelsAttributeStateUpdater))]
 
 public class VectorLabelsAttributeStateUpdater : AttributeStateUpdater<VectorLabelsAttribute>
 {
+    private VectorLabelsAttribute vectorLabelsAttribute;
+    private Dictionary<string, string> previousLabels;
+
     protected override void Initialize()
     {
-        Property.Tree.OnPropertyValueChanged += (property, selectionIndex) => Property.RefreshSetup();
+        vectorLabelsAttribute = Property.GetAttribute<VectorLabelsAttribute>();
+        previousLabels = new Dictionary<string, string>();
+
+        foreach (var child in Property.Children)
+        {
+            previousLabels[child.Name] = ValueResolver
+                .GetForString(Property, vectorLabelsAttribute.Labels[child.Name])
+                .GetValue();
+        }
+    }
+
+    public override void OnStateUpdate()
+    {
+        foreach (var child in Property.Children)
+        {
+            var label = ValueResolver
+                .GetForString(Property, vectorLabelsAttribute.Labels[child.Name])
+                .GetValue();
+
+            if (label != previousLabels[child.Name])
+            {
+                previousLabels[child.Name] = label;
+                Property.RefreshSetup();
+            }
+        }
     }
 }
