5aaedcf4e107ca4f9763e0c4eccb52dbdf268b6a
Antonio Rafael Antunes Miranda <36130345+Schwapo@users.noreply.github.com>
2021-06-29 12:07:59 +0200

    Update VectorLabelsAttributeProcessor.cs

--- VectorLabels/VectorLabelsAttributeProcessor.cs
+++ VectorLabels/VectorLabelsAttributeProcessor.cs
@@ -1,23 +1,24 @@
 #if UNITY_EDITOR
-using System;
-using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using UnityEngine;
-
 using Sirenix.OdinInspector.Editor;
 using Sirenix.OdinInspector.Editor.ValueResolvers;
+using System;
+using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 public class VectorLabelsAttributeProcessor : OdinAttributeProcessor
 {
+    private VectorLabelsAttribute vectorLabelsAttribute;
+
     public override bool CanProcessSelfAttributes(InspectorProperty property)
-        => property.Parent?.Attributes.HasAttribute<VectorLabelsAttribute>() ?? false;
+    {
+        vectorLabelsAttribute = property.Parent?.GetAttribute<VectorLabelsAttribute>();
+        return vectorLabelsAttribute != null;
+    }
 
     public override void ProcessSelfAttributes(InspectorProperty property, List<Attribute> attributes)
-    {
-        var attribute = property.Parent.GetAttribute<VectorLabelsAttribute>();
-        ChangeLabel(property.Parent, attribute.labels[property.Name], ref attributes);
-    }
+        => ChangeLabel(property.Parent, vectorLabelsAttribute.Labels[property.Name], ref attributes);
 
     private void ChangeLabel(InspectorProperty property, string value, ref List<Attribute> attributes)
     {
